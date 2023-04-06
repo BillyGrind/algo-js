7f0e665e7fa35eba474d29a09aaeaef31b12cecc
Anthony <denayeranthony1782@gmail.com>
2023-04-06 10:42:48 +0200

    ex 2.1

---
+++ js/2.1.js
@@ -0,0 +1,10 @@
+// let age = prompt("What's your age ?");
+
+age = window.prompt("What's your age ?");
+
+if (age >= 18) {
+  console.log("You're an adult");
+} else {
+  console.log("You're not yet an adult");
+}
+
