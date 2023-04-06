2c4fa897b00407305c9eeca90d5e8b391ad4bd66
Anthony <denayeranthony1782@gmail.com>
2023-04-06 11:31:16 +0200

    Ex 2.3

--- js/2.3.js
+++ js/2.3.js
@@ -1,12 +1,22 @@
+// Afficher les pairs jusqu'a 100
 
-let e = 0;
-
-while (e<100){
-    e ++;
-    console.log(e);
+for(i=0;i<=100;i++){
+    if(i%2==0){
+        console.log(i);
+    }
 }
 
- for(let i=0; i<=50 ;i++){
-     console.log(i);
- }
+
+// Le BONUS 
+
+// let e = 0;
+
+// while (e<100){
+//     e ++;
+//     console.log(e);
+// }
+
+//  for(let i=0; i<=50 ;i++){
+//      console.log(i);
+//  }
 
