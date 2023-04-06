eb6f482e7a9b10178123a085e8a47cba14dab47b
Anthony <denayeranthony1782@gmail.com>
2023-04-06 10:57:20 +0200

    Ex 2.2

---
+++ js/2.2.js
@@ -0,0 +1,12 @@
+let min= prompt("Choose a Min Number");
+let max=prompt("Choose a MaxXx Number");
+let current=prompt("..And a Kurent Number");
+
+
+if(current>min && current<max){
+    alert("The Kurent Number ( "+current+" ) is between" + min +" and "+max );
+}else if (min>max){
+    alert("You understand anything USER !")
+} else{
+    alert("Program Fail")
+}
