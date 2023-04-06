5de3e33c7e5bc35a1115d76f321d61d1d161272f
Anthony <denayeranthony1782@gmail.com>
2023-04-06 12:08:52 +0200

    Ex 2.6

---
+++ js/2.6.js
@@ -0,0 +1,24 @@
+let number = prompt("Enter a number between 1 and 7 plz");
+
+if((number>=1) && (number<=7)){
+    if (number == 1) {
+        console.log("Monday");
+      } else if (number == 2) {
+        console.log("Tuesday");
+      } else if (number == 3) {
+        console.log("Mercredi");
+      } else if (number == 4) {
+        console.log("Jeudi");
+      } else if (number == 5) {
+        console.log("Vendredi");
+      } else if (number == 6) {
+        console.log("Samedi");
+      } else if (number == 7) {
+        console.log("Dimanche");
+      }
+}else{
+    alert("ENTRE UN ET SEPT g di");
+}
+
+
+
